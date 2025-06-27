fa2e12f58e107b44dfb90970ad08a083df07efce
庞晓飞 <398862770@qq.com>
2025-06-27 17:25:48 +0800

    安装scss和tailwind

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@renderer/assets/styles/global.scss'
+import '@renderer/assets/styles/tailwind.css'
 
 createApp(App).mount('#app')
